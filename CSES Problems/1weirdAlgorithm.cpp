367d1de6c4ecef2ff3441c5bd4b8b1d6d15851ea
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-02-11 23:15:51 +0530

    Fix base case comment in recursion function

--- CSES Problems/1weirdAlgorithm.cpp
+++ CSES Problems/1weirdAlgorithm.cpp
@@ -6,7 +6,7 @@ void recursionFunction(int n){
     cout << n << endl;
 
     if (n == 1){  
-        return; //Base case stop recursion
+        return; 
     }
     else if(n%2 == 1){
         n = n*3 + 1;
