8ff9d052006b92d75127f80f23eb1e529fc5c7d4
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-03-04 14:43:03 +0530

    Rename CSES Problems to CSES_Problems to fix build path issues

---
+++ CSES_Problems/1weirdAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void recursionFunction(int n){
+    cout << n << endl;
+
+    if (n == 1){  
+        return; 
+    }
+    else if(n%2 == 1){
+        n = n*3 + 1;
+    }
+    else{
+        n = n/2;
+        
+    }
+
+    recursionFunction(n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    recursionFunction(n);
+    return 0;
+}
