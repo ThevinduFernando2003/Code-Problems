f96c3b9b612085d87044c21b85c7a660bdb2b57d
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:36 +0530

    Add LeetCode 1679 Max Number of K-Sum Pairs.

---
+++ Leetcode/1679maxNumberOfKSumPairs.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int i = 0, j = (int)nums.size() - 1, ans = 0;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == k) {
+                ++ans;
+                ++i;
+                --j;
+            } else if (sum < k) ++i;
+            else --j;
+        }
+        return ans;
+    }
+};
