83aa8ec2528bbab0d84dfb3824dc120cde3e027f
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:40 +0530

    Add LeetCode 735 Asteroid Collision.

---
+++ Leetcode/735asteroidCollision.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for (int a : asteroids) {
+            bool alive = true;
+            while (alive && a < 0 && !st.empty() && st.back() > 0) {
+                if (st.back() < -a) st.pop_back();
+                else {
+                    if (st.back() == -a) st.pop_back();
+                    alive = false;
+                }
+            }
+            if (alive) st.push_back(a);
+        }
+        return st;
+    }
+};
