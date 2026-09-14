8f58c272c1a390e9eb42d63455446ea702865b37
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:10 +0530

    Add LeetCode 215 Kth Largest Element in an Array.

---
+++ Leetcode/215kthLargestElementInAnArray.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <queue>
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int x : nums) {
+            pq.push(x);
+            if ((int)pq.size() > k) pq.pop();
+        }
+        return pq.top();
+    }
+};
