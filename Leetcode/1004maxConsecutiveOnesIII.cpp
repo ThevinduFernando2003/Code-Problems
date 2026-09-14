b07d11fe0557c1c05c0e0328f61ca30fe2aa377a
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:37 +0530

    Add LeetCode 1004 Max Consecutive Ones III.

---
+++ Leetcode/1004maxConsecutiveOnesIII.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, zeros = 0, best = 0;
+        for (int right = 0; right < (int)nums.size(); ++right) {
+            if (nums[right] == 0) ++zeros;
+            while (zeros > k) {
+                if (nums[left] == 0) --zeros;
+                ++left;
+            }
+            best = max(best, right - left + 1);
+        }
+        return best;
+    }
+};
