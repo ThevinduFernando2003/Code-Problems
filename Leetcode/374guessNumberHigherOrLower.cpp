cfd817549b1d24305d18244fce7fd26380bcb3dd
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:30 +0530

    Add LeetCode 374 Guess Number Higher or Lower.

---
+++ Leetcode/374guessNumberHigherOrLower.cpp
@@ -0,0 +1,16 @@
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int lo = 1, hi = n;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int res = guess(mid);
+            if (res == 0) return mid;
+            if (res < 0) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+};
