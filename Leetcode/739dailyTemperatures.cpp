2ace509dbaf194a397e1c89739658bc06c5bad0b
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:34 +0530

    Add LeetCode 739 Daily Temperatures.

---
+++ Leetcode/739dailyTemperatures.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = (int)temperatures.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
