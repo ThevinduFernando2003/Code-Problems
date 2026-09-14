b352f5e813cbaf63396d1a5e6a6d4696acdd1c29
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:33 +0530

    Add LeetCode 62 Unique Paths.

---
+++ Leetcode/62uniquePaths.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);
+        for (int r = 1; r < m; ++r) {
+            for (int c = 1; c < n; ++c) dp[c] += dp[c - 1];
+        }
+        return dp[n - 1];
+    }
+};
