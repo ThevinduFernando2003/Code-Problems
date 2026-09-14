b44648ca93491e744943132a6a2bbda3e642ec5b
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:03 +0530

    Add LeetCode 933 Number of Recent Calls.

---
+++ Leetcode/933numberOfRecentCalls.cpp
@@ -0,0 +1,13 @@
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+    queue<int> q;
+public:
+    RecentCounter() = default;
+    int ping(int t) {
+        q.push(t);
+        while (!q.empty() && q.front() < t - 3000) q.pop();
+        return (int)q.size();
+    }
+};
