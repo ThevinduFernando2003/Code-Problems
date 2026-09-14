76d4bd88ea76a74b8366f5ffc1fdbc5058b52177
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:29:33 +0530

    Add LeetCode 714 Best Time to Buy and Sell Stock with Fee.

---
+++ Leetcode/714bestTimeToBuyAndSellStockWithFee.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int hold = -prices[0], cash = 0;
+        for (int i = 1; i < (int)prices.size(); ++i) {
+            hold = max(hold, cash - prices[i]);
+            cash = max(cash, hold + prices[i] - fee);
+        }
+        return cash;
+    }
+};
