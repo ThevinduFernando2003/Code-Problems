faa226565ae31f936b40b01715310e22783b08d3
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:39 +0530

    Add LeetCode 2390 Removing Stars From a String.

---
+++ Leetcode/2390removingStarsFromAString.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeStars(string s) {
+        string st;
+        for (char c : s) {
+            if (c == '*') {
+                if (!st.empty()) st.pop_back();
+            } else st.push_back(c);
+        }
+        return st;
+    }
+};
