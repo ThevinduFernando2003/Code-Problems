19be6c028b02de292e44778de23746b24afc9a1a
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:37 +0530

    Add LeetCode 1493 Longest Subarray After Deleting One.

---
+++ Leetcode/1493longestSubarrayOfOnesAfterDeletingOne.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int left = 0, zeros = 0, best = 0;
+        for (int right = 0; right < (int)nums.size(); ++right) {
+            if (nums[right] == 0) ++zeros;
+            while (zeros > 1) {
+                if (nums[left] == 0) --zeros;
+                ++left;
+            }
+            best = max(best, right - left);
+        }
+        return best;
+    }
+};
