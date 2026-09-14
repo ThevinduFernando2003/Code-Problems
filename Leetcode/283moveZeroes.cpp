bdf8449d90e5c87f132c474ebd276b76a7fd8a10
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:35 +0530

    Add LeetCode 283 Move Zeroes.

---
+++ Leetcode/283moveZeroes.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int write = 0;
+        for (int x : nums) if (x != 0) nums[write++] = x;
+        while (write < (int)nums.size()) nums[write++] = 0;
+    }
+};
