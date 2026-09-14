f61353b762f8419d69c1122693e049e320ca3dad
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-14 13:28:32 +0530

    Add LeetCode 1071 GCD of Strings.

---
+++ Leetcode/1071greatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        return str1.substr(0, gcd((int)str1.size(), (int)str2.size()));
+    }
+};
