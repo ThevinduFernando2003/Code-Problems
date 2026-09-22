1c31a2ff15146057bfcec67b305ee836ec5ff6c8
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:12 +0530

    Add pseudocode to LeetCode 345 reverse vowels of a string.

--- Leetcode/345reverseVowelsOfAString.cpp
+++ Leetcode/345reverseVowelsOfAString.cpp
@@ -2,6 +2,23 @@
 #include <cctype>
 using namespace std;
 
+/*
+LeetCode 345 - Reverse Vowels of a String
+
+Reverse only the vowels in s (a, e, i, o, u, case insensitive).
+Consonants stay in place.
+
+Pseudocode
+----------
+i := 0, j := |s| - 1
+while i < j:
+    advance i until it lands on a vowel
+    retreat j until it lands on a vowel
+    swap s[i] and s[j]
+    i := i + 1, j := j - 1
+return s
+*/
+
 class Solution {
     static bool isVowel(char c) {
         c = (char)tolower((unsigned char)c);
