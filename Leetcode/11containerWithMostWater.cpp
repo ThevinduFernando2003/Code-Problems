11980cf11cf5eb43460a6c34ee34f2f2725caff2
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:14 +0530

    Add pseudocode to LeetCode 11 container with most water.

--- Leetcode/11containerWithMostWater.cpp
+++ Leetcode/11containerWithMostWater.cpp
@@ -2,6 +2,22 @@
 #include <algorithm>
 using namespace std;
 
+/*
+LeetCode 11 - Container With Most Water
+
+Choose two vertical lines so the axis-aligned container they form
+holds the most water.
+
+Pseudocode
+----------
+i := 0, j := n - 1, best := 0
+while i < j:
+    best := max(best, min(height[i], height[j]) * (j - i))
+    if height[i] < height[j]: i := i + 1
+    else: j := j - 1
+return best
+*/
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
