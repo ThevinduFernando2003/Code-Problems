cac647c6ccfeb227f0b124f038bd2c236aee80b8
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:13 +0530

    Add pseudocode to LeetCode 238 product of array except self.

--- Leetcode/238productOfArrayExceptSelf.cpp
+++ Leetcode/238productOfArrayExceptSelf.cpp
@@ -1,6 +1,25 @@
 #include <vector>
 using namespace std;
 
+/*
+LeetCode 238 - Product of Array Except Self
+
+Return an array where answer[i] is the product of all nums values
+except nums[i]. Do not use division.
+
+Pseudocode
+----------
+left := 1
+for i from 0 to n-1:
+    answer[i] := left
+    left := left * nums[i]
+right := 1
+for i from n-1 down to 0:
+    answer[i] := answer[i] * right
+    right := right * nums[i]
+return answer
+*/
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
