d6f7dde3e155a71b8327f65e30f93cf00642c969
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:15 +0530

    Add pseudocode to LeetCode 724 find pivot index.

--- Leetcode/724findPivotIndex.cpp
+++ Leetcode/724findPivotIndex.cpp
@@ -2,6 +2,22 @@
 #include <numeric>
 using namespace std;
 
+/*
+LeetCode 724 - Find Pivot Index
+
+Return the leftmost index where the sum of values to the left equals
+the sum of values to the right. Return -1 if none exists.
+
+Pseudocode
+----------
+total := sum(nums)
+left := 0
+for i from 0 to n-1:
+    if left == total - left - nums[i]: return i
+    left := left + nums[i]
+return -1
+*/
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
