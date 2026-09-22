82c3acce954d12dbf490073a83b3a7b0d99975ab
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:13 +0530

    Add pseudocode to LeetCode 334 increasing triplet subsequence.

--- Leetcode/334increasingTripletSubsequence.cpp
+++ Leetcode/334increasingTripletSubsequence.cpp
@@ -2,6 +2,21 @@
 #include <climits>
 using namespace std;
 
+/*
+LeetCode 334 - Increasing Triplet Subsequence
+
+Return true if there exist i < j < k with nums[i] < nums[j] < nums[k].
+
+Pseudocode
+----------
+first := +inf, second := +inf
+for each x in nums:
+    if x <= first: first := x
+    else if x <= second: second := x
+    else: return true   // x > first and x > second
+return false
+*/
+
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
