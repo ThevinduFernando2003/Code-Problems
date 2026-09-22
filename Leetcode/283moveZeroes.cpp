a871e3d64cd2350c9828648bcd092bfc4a5f298b
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:11 +0530

    Add pseudocode to LeetCode 283 move zeroes.

--- Leetcode/283moveZeroes.cpp
+++ Leetcode/283moveZeroes.cpp
@@ -1,6 +1,20 @@
 #include <vector>
 using namespace std;
 
+/*
+LeetCode 283 - Move Zeroes
+
+Move all zeroes to the end of the array while keeping the relative
+order of the non-zero values. Do it in place.
+
+Pseudocode
+----------
+write := 0
+for each x in nums:
+    if x != 0: nums[write] := x, write := write + 1
+fill nums[write .. end] with 0
+*/
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
