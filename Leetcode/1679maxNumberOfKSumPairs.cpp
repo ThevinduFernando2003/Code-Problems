4c34927edd359af1dfe94881a34937bee447a01d
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:14 +0530

    Add pseudocode to LeetCode 1679 max number of k-sum pairs.

--- Leetcode/1679maxNumberOfKSumPairs.cpp
+++ Leetcode/1679maxNumberOfKSumPairs.cpp
@@ -2,6 +2,23 @@
 #include <algorithm>
 using namespace std;
 
+/*
+LeetCode 1679 - Max Number of K-Sum Pairs
+
+Count the maximum number of operations that remove two numbers summing
+to k.
+
+Pseudocode
+----------
+sort nums
+i := 0, j := n - 1, ans := 0
+while i < j:
+    if nums[i] + nums[j] == k: ans := ans + 1, i := i + 1, j := j - 1
+    else if sum < k: i := i + 1
+    else: j := j - 1
+return ans
+*/
+
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
