a016889be71f560e447a8334db3f9850b3c4699a
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:14 +0530

    Add pseudocode to LeetCode 392 is subsequence.

--- Leetcode/392isSubsequence.cpp
+++ Leetcode/392isSubsequence.cpp
@@ -1,6 +1,20 @@
 #include <string>
 using namespace std;
 
+/*
+LeetCode 392 - Is Subsequence
+
+Return true if s can be formed by deleting some (or no) characters
+from t without changing the order of the remaining characters.
+
+Pseudocode
+----------
+i := 0
+for each character c in t:
+    if i < |s| and c == s[i]: i := i + 1
+return i == |s|
+*/
+
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
