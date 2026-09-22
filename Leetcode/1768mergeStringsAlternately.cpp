9d936b68321ac11e2198b8b5774fa4fe5c467c06
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:11 +0530

    Add pseudocode to LeetCode 1768 merge strings alternately.

--- Leetcode/1768mergeStringsAlternately.cpp
+++ Leetcode/1768mergeStringsAlternately.cpp
@@ -1,6 +1,21 @@
 #include <string>
 using namespace std;
 
+/*
+LeetCode 1768 - Merge Strings Alternately
+
+Build a string by taking one character from word1, then one from word2,
+repeating until both inputs are exhausted.
+
+Pseudocode
+----------
+i := 0, j := 0, ans := empty
+while i < |word1| or j < |word2|:
+    if i < |word1|: append word1[i], i := i + 1
+    if j < |word2|: append word2[j], j := j + 1
+return ans
+*/
+
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
