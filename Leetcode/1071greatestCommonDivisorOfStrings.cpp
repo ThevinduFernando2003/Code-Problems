cab21c3edd02aff93822a2e2a563d97f43c15c1d
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:12 +0530

    Add pseudocode to LeetCode 1071 GCD of strings.

--- Leetcode/1071greatestCommonDivisorOfStrings.cpp
+++ Leetcode/1071greatestCommonDivisorOfStrings.cpp
@@ -2,6 +2,18 @@
 #include <numeric>
 using namespace std;
 
+/*
+LeetCode 1071 - Greatest Common Divisor of Strings
+
+Return the longest string X such that X repeats to form both str1 and
+str2. If no such string exists, return empty.
+
+Pseudocode
+----------
+if str1 + str2 != str2 + str1: return ""
+return prefix of str1 of length gcd(|str1|, |str2|)
+*/
+
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
