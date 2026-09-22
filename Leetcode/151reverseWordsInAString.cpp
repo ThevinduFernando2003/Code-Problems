9836505a1903fccc36ddd54b5d194b02980a53ef
Thevindu Fernando <thevindufernando0315@gmail.com>
2026-09-22 16:41:13 +0530

    Add pseudocode to LeetCode 151 reverse words in a string.

--- Leetcode/151reverseWordsInAString.cpp
+++ Leetcode/151reverseWordsInAString.cpp
@@ -3,6 +3,19 @@
 #include <vector>
 using namespace std;
 
+/*
+LeetCode 151 - Reverse Words in a String
+
+Reverse the order of words in s. Collapse extra spaces so the result
+has single spaces between words and no leading/trailing space.
+
+Pseudocode
+----------
+split s on whitespace into words
+join words from last to first with a single space
+return the joined string
+*/
+
 class Solution {
 public:
     string reverseWords(string s) {
